0f32532268fa4af6b57a927ea4d0a498ef5043bd
Vishal Singh <vishalpratap906@gmail.com>
2024-10-30 17:40:48 +0530

    Deployment commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import authReducer from '../features/auth/authSlice.js'
+import taskReducer from '../features/tasks/taskSlice.js'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    auth: authReducer,
+    tasks: taskReducer
   },
 });
