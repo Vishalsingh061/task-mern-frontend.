ba8d8d0fdea751f1e42841e361778d24f0b79a8d
Vishal Singh <vishalpratap906@gmail.com>
2024-10-30 18:43:13 +0530

    Cors Added

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = '/api/users/'
+const API_URL = 'https://task-mern-backend-9fzw.onrender.com/api/users/'
 const register = async (userData) => {
     const response = await axios.post(API_URL, userData)
     if (response.data) {
--- src/features/tasks/taskService.js
+++ src/features/tasks/taskService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = '/api/tasks/'
+const API_URL = 'https://task-mern-backend-9fzw.onrender.com/api/tasks/'
 
 const createTask = async (taskData, token) => {
     const config = {
